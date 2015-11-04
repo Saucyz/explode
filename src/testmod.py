b7e208b96ffe7efc2d7377f4b21cd4d9b47537cc
Saucyz <richardgleong@gmail.com>
2015-11-04 10:49:56 -0800

    Simply prints that main ran correctly on console.

--- src/testmod.py
+++ src/testmod.py
@@ -1,3 +1,12 @@
-import module.py
+import module
 
+#A test module
+class Testmod:
+	#Constructor
+	def __init__(self):
+		#Can't have function do nothing, uses pass
+		pass
 
+def main():
+	print "main() ran correctly.\n"
+main()
