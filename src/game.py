eee8220040465fc5947e8d840df2a12c40ec3e20
Busybob <andrew8b2@gmail.com>
2015-11-08 15:03:47 -0800

    Additional functions to connect game and bomb

--- src/game.py
+++ src/game.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python
+import bomb
+
+MAX_STRIKES = 3
+
+class Game:
+	def __init__(self, level):
+		self.bomb = Bomb(30)
+		self.totalStrikes = 0
+		self.won = False
+
+	def checkGameState(self):
+		x = bomb.checkModStates()
+		if x > 0:
+			self.totalStrikes += x
+		elif x == 0:
+			self.won = True
+			self.win()
+		else:
+			self.checkLose()
+
+
+	def win(self):
+		#print win screen and exit game
+		pass
+
+	def checkLose(self):
+		#print lose screen and exit game
+		pass
--- src/testbomb.py
+++ src/testbomb.py
@@ -1,3 +1,16 @@
 import bomb
 
+#testing main function
+def main():
+	b = bomb.Bomb(3)
+	b.start()
+	
+	#timer = BombTimer(100)
+	#timer.countdown()
 
+	#bomb = Bomb()
+	#bomb.populate()
+
+	#bomb.moduleList[0].testFunction()
+
+main()
