f3fa90fb8450b9128424e6c539a6d037074ba4cf
Saucyz <richardgleong@gmail.com>
2015-11-12 16:36:58 -0800

    Small tweaks

--- src/main.py
+++ src/main.py
@@ -4,4 +4,21 @@ import init
 import game
 import display
 import module
+import sys, random
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QWidget
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor
 
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    painter = QPainter()
+    
+
+    sys.exit(app.exec_())
