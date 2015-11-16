613f6a90c58262ad9997d9e48169565160ebc162
BusyBob <andrew8b2@gmail.com>
2015-11-16 20:52:59 +0000

    fixes for serial

--- src/serialCom.py
+++ src/serialCom.py
@@ -1,20 +1,20 @@
 import serial
 import time
 
-port = serial.Serial('/dev/ttyUSB0', baudrate = 115200, timeout = 3.0)
-
-a = b"Serial port connected and running"
-print a
-
 #Holds the serial loop to send and recieve
 #the states of the DE2 module and bomb
-def serialCom(self):
-	def serialWrite(self, string state):
-		port.write(state)
+class serialCom:
+	def __init__(self):
+		self.port = serial.Serial('/dev/ttyUSB0', baudrate = 115200, timeout = 0.01)
+		a = b"Serial port connected and running"
+		print a
+
+	def serialWrite(self, state):
+		self.port.write(state)
 
 	def serialRead(self):
-		rcv = port.read(1)
-		print "I got " + str(len(rcv)) + " characters " + rcv 
+		rcv = self.port.read(1)
+		#time.sleep(0.01)
+		#print "I got " + str(len(rcv)) + " characters " + rcv 
 		return rcv
-		time.sleep(0.5)
 
